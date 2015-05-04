68a3e52970fa0ee70906993330c54dbc25f67091
Keith Bingman <keith@keithbingman.com>
2015-05-04 10:33:16 -0700

    added flight

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,40 @@
-var Flightjs = require('flightjs');
-var hello = require('helloworld');
+window.$ = require('jquery');
+
+// var Flightjs = require('flightjs');
+var Immutable = require('immutable');
+// var hello = require('helloworld');
+var helloWorldUI = require('helloworld/ui');
+helloWorldUI.attachTo(document);
 
 var start = +new Date();
 
-hello.render('Mars');
+// hello.render('Hanna');
+// hello.typewriter('Welcome to the Epsilon Eridani system. What would you like to do next?');
+
 console.log(+new Date() - start);
 
-window.Flightjs = Flightjs;
+// window.Flightjs = Flightjs;
+// window.Immutable = Immutable;
+
+// var state = { color: '', value: 0 };
+//
+// function updateUI() {
+//   localStorage.setItem('state', JSON.stringify(state));
+//   $('#colored-counter').css('color', state.color);
+//   $('#colored-counter').html(state.value);
+// }
+//
+// $(function () {
+//   state = JSON.parse(localStorage.getItem('state')) || state;
+//   updateUI();
+// });
+//
+// $('#color').on('keyup', function () {
+//     state.color = this.value;
+//     updateUI();
+// });
+//
+// $('#inc').on('click', function () {
+//     state.value++;
+//     updateUI();
+// });
